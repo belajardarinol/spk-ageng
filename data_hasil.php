ae81562a75b9d46070f259bacca82ccc3609ae46
failamir <38488480+failamir@users.noreply.github.com>
2022-02-10 17:52:47 +0700

    Update data_hasil.php

--- data_hasil.php
+++ data_hasil.php
@@ -75,10 +75,10 @@ session_start();
 								$queryutama3 = mysql_query("SELECT  *  FROM  hasil order by nt desc");
 								// print_r(mysql_fetch_array($queryutama3));
 								$n = 1;
-								$data1 = mysql_fetch_array($queryutama3);
-								foreach ($data1 as $data2) {
-									$data3 = (array)$data2;die;
-								// while ($data3 = mysql_fetch_array($queryutama3)) {
+								// $data1 = mysql_fetch_array($queryutama3);
+								// foreach ($data1 as $data2) {
+								// 	$data3 = (array)$data2;die;
+								while ($data3 = mysql_fetch_array($queryutama3)) {
 									
 									$kar = mysql_fetch_array(mysql_query("select * from food where id_makanan='$data3[idproses]'"));
 								?>
